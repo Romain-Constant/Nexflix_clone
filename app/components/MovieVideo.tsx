c7ba8a3061aff642a5b00c58c8289733dbca3ddb
Romain-Constant <romain.constant59@gmail.com>
2024-01-11 15:36:37 +0100

    :lipstick:all pages and components done

--- app/components/MovieVideo.tsx
+++ app/components/MovieVideo.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
 import prisma from "../utils/db";
+import MovieButtons from "./MovieButtons";
 
 async function getData() {
   const data = await prisma.movie.findFirst({
@@ -12,6 +12,7 @@ async function getData() {
       duration: true,
       id: true,
       age: true,
+      youtubeString: true,
     },
   });
   return data;
@@ -37,8 +38,16 @@ export default async function MovieVideo() {
         </h1>
         <p className="text-white text-lg mt-5 line-clamp-3">{data?.overview}</p>
         <div className="flex gap-x-3 mt-4">
-          <Button>See more</Button>
-          <Button>Learn more</Button>
+          <MovieButtons
+            age={data?.age as number}
+            duration={data?.duration as number}
+            id={data?.id as number}
+            overview={data?.overview as string}
+            releaseDate={data?.release as number}
+            title={data?.title as string}
+            youtubeUrl={data?.youtubeString as string}
+            key={data?.id}
+          />
         </div>
       </div>
     </div>
